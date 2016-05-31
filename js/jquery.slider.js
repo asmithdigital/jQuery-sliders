be5b7a1fe7b03dd166c0cc6688a6036c624e6218
Andrew Smith <andrew@asmithdigital.com>
2016-05-31 14:34:57 +0930

    Fix linting
    Signed-off-by: Andrew Smith <andrew@asmithdigital.com>

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -23,7 +23,7 @@
  */
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;( function( $, window, document, undefined ) {
+;(function ($, window, document, undefined) {
   "use strict";
 
   // undefined is used here as the undefined global variable in ECMAScript 3 is
@@ -47,41 +47,41 @@
     };
 
   // The actual plugin constructor
-  function Plugin ( element, options ) {
+  function Plugin(element, options) {
     this.element = element;
 
     // jQuery has an extend method which merges the contents of two or
     // more objects, storing the result in the first object. The first object
     // is generally empty as we don't want to alter the default options for
     // future instances of the plugin
-    this.settings = $.extend( {}, defaults, options );
+    this.settings = $.extend({}, defaults, options);
     this._defaults = defaults;
     this._name = pluginName;
     this.init();
   }
 
   // Avoid Plugin.prototype conflicts
-  $.extend( Plugin.prototype, {
-    init: function() {
+  $.extend(Plugin.prototype, {
+    init: function () {
       var self = this,
-          $el = $(this.element),
-          ul = $el.find("ul"),
-          slide_count = ul.children().length, // array of slides
-          slide_width = slide_count * 100.00, // sets the UL width to a multiple of each slide * 100%
-          slide_height = 400, // max height of the slide images
-          new_slide_height = slide_height,
-          thumb_height = slide_height,
-          slide_width_pc = 100.0 / slide_count, // % width of each slide
-          slide_index, // will be used to define the active slide
-          first_slide = ul.find("li:first-child"),
-          last_slide = ul.find("li:last-child"),
-          thumbsArray = [], // initially empty thumbs array
+        $el = $(this.element),
+        ul = $el.find("ul"),
+        slide_count = ul.children().length, // array of slides
+        slide_width = slide_count * 100.00, // sets the UL width to a multiple of each slide * 100%
+        slide_height = 400, // max height of the slide images
+        new_slide_height = slide_height,
+        thumb_height = slide_height,
+        slide_width_pc = 100.0 / slide_count, // % width of each slide
+        slide_index, // will be used to define the active slide
+        first_slide = ul.find("li:first-child"),
+        last_slide = ul.find("li:last-child"),
+        thumbsArray = [], // initially empty thumbs array
 
-          previous_button_class = self.settings.previous_button_class,
-          next_button_class = self.settings.next_button_class,
-          thumbs_class = self.settings.thumbs_class,
-          max_slide_thumbs = self.settings.max_slide_thumbs, // will decide the widths of each thumb
-          active_class = self.settings.active_class;
+        previous_button_class = self.settings.previous_button_class,
+        next_button_class = self.settings.next_button_class,
+        thumbs_class = self.settings.thumbs_class,
+        max_slide_thumbs = self.settings.max_slide_thumbs, // will decide the widths of each thumb
+        active_class = self.settings.active_class;
 
 
       // Slide function
@@ -100,7 +100,7 @@
         // so that the animation can appear smmoth because the slide exists
         // - otherwise there will be a white background flash without image
         // then the ul margin left gets reset to the actual relevent slide (first/last)
-        ul.animate({"margin-left": margin_left_pc}, 400, function() {
+        ul.animate({"margin-left": margin_left_pc}, 400, function () {
 
           console.log('Initial animation complete');
 
@@ -109,7 +109,7 @@
           // ul margin left set to slide array length * -100%
           // and set integer to the last slide (not the appended slide)
           // ie, 3 slides, margin-left -300%;
-          if(new_slide_index < 0) {
+          if (new_slide_index < 0) {
             var leftMagrinSetTo = (slide_count) * (-100);
             ul.css("margin-left", leftMagrinSetTo + '%');
             new_slide_index = slide_count - 1;
@@ -121,7 +121,7 @@
           // - at the last slide and click the next button
           // reset the ul margin left to -100% (first slide)
           // and reset integer to 0
-          else if(new_slide_index >= slide_count) {
+          else if (new_slide_index >= slide_count) {
             ul.css("margin-left", "-100%");
             new_slide_index = 0;
             console.log('New left margin set to: -100%');
@@ -143,11 +143,11 @@
       var styleActiveThumb = function (newIndex) {
 
         // loop through thumbs and compare index with passed in newIndex
-        thumbItem.each(function(i) {
+        thumbItem.each(function (i) {
 
           // Style the active thumb
           // active thumb is the thumb index which matches the slide index
-          if (i == newIndex) {
+          if (i === newIndex) {
             thumbItem.css("opacity", ".7");
             $(this).css("opacity", "1");
           }
@@ -165,8 +165,8 @@
       // remove the active class from the newly cloned last slide
       // if the first slide html class was set to active
       last_slide = ul.find("li:last-child");
-      if (last_slide.attr( "class" ) === active_class) {
-        last_slide.removeAttr( "class" );
+      if (last_slide.attr("class") === active_class) {
+        last_slide.removeAttr("class");
       }
 
       // move the ul to the left equal to the width of the newly prepended
@@ -180,15 +180,15 @@
       // based on the % width of each index in the loop
       // which matches the slide width array
       // - this removes the need for a width in CSS
-      ul.find("li").each(function(indx) {
+      ul.find("li").each(function (indx) {
 
         // store the images in a thumbs array
         var image = $(this).find('img').attr('src');
         thumbsArray.push(image);
 
         var left_percent = (slide_width_pc * indx) + "%";
-        $(this).css({"left":left_percent});
-        $(this).css({width:(100 / slide_count) + "%"});
+        $(this).css({left: left_percent});
+        $(this).css({width: (100 / slide_count) + "%"});
 
         // Set the active slide index
         if ($(this).hasClass(self.settings.active_class)) {
@@ -204,7 +204,9 @@
 
         if (new_slide_height < slide_height) {
           $el.height(new_slide_height);
-        } else {
+        }
+
+        else {
           $el.height(slide_height);
         }
 
@@ -213,13 +215,13 @@
       // Prev/next buttons init
       // ==========================
       // Pass in negative 1 to index integer to slide()
-      $("." + previous_button_class).click(function() {
+      $("." + previous_button_class).click(function () {
         console.log("-- prev button clicked");
         slide(slide_index - 1);
       });
 
       // Pass in positive 1 to index integer to slide()
-      $("." + next_button_class).click(function() {
+      $("." + next_button_class).click(function () {
         console.log("++ next button clicked");
         slide(slide_index + 1);
       });
@@ -228,11 +230,11 @@
       // ==========================
       // slice the first and last items from the thumbs array
       // because we cloned them above and we dont need them for the thumbs
-      var thumbsArray = thumbsArray.slice( 1, -1 );
+      thumbsArray = thumbsArray.slice(1, -1);
 
       // Build the thumbs markup from the thumbs array
       var thumbList = "<div class='" + thumbs_class + "'><ul class='" + thumbs_class + "__list'>";
-      for(var i=0; i< thumbsArray.length; i++) {
+      for (var i = 0; i < thumbsArray.length; i++) {
         thumbList += "<li class='" + thumbs_class + "__item'>";
         thumbList += "<img src='" + thumbsArray[i] + "' class='" + thumbs_class + "__image'>";
         thumbList += "</li>";
@@ -254,7 +256,7 @@
 
       // pass the thumb index as new slide index
       // to the slide() function
-      thumbItem.click(function(){
+      thumbItem.click(function () {
         var thumbIndex = $(this).index();
         slide(thumbIndex);
         console.log('clicked ' + thumbIndex);
@@ -262,10 +264,10 @@
 
       // Find the shortest thumbnail image and sets the parent div height
       // get the height of each image
-      thumbItem.each(function() {
+      thumbItem.each(function () {
         // get the height of each image
         var imageHeight = $(this).find("img").height();
-        if(imageHeight < thumb_height) {
+        if (imageHeight < thumb_height) {
           thumb_height = imageHeight;
         }
 
@@ -280,17 +282,17 @@
       styleActiveThumb(slide_index);
     }
 
-  } );
+  });
 
   // A really lightweight plugin wrapper around the constructor,
   // preventing against multiple instantiations
-  $.fn[ pluginName ] = function( options ) {
-    return this.each( function() {
-      if ( !$.data( this, "plugin_" + pluginName ) ) {
-        $.data( this, "plugin_" +
-        pluginName, new Plugin( this, options ) );
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" +
+          pluginName, new Plugin(this, options));
       }
-    } );
+    });
   };
 
-} )( jQuery, window, document );
+})(jQuery, window, document);
