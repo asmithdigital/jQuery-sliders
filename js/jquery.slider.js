d16e8ac9f356c2cf06f4365f70dc36a35243cd84
Andrew Smith <andrew@asmithdigital.com>
2016-06-07 14:41:00 +0930

    Remove unused and comments, improve methods
    Signed-off-by: Andrew Smith <andrew@asmithdigital.com>

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -3,40 +3,29 @@
  *
  *  Generates thumbnails and slides continuously through slides.
  *
- *  @TODO:
- *  - setInterval() for auto sliding on page load
- *  - add touch events http://hammerjs.github.io/getting-started/
- *  - use CSS transitions for animations
- *  - resolve responsive layouts
- *
  *  Options:
  *  - max_number_slides (integer): max number of slides and thumbs
  *  - active_class (string): sets the active slide and thumb
  *  - previous_button_class (string): previous button class name
  *  - next_button_class (string): next button class name
  *  - thumbs_class (string): class name for generated thumbs markup
- *  - onAdLoaded: (object): @TODO
  *
  *  Made by Andrew Smith andrew.smith03@adelaide.edu.au
  *
- *  jquery-boilerplate - v4.0.0
- *  A jump-start for jQuery plugins development.
- *  http://jqueryboilerplate.com
- *
- *  Made by Zeno Rocha
- *  Under MIT License
  */
-;(function ($) {
+(function ($) {
   "use strict";
   var pluginName = "ua_slidie";
   var defaults = {
     max_slide_thumbs: 5,
     auto_slide: true,
-    autoslide_speed: 3000,
+    autoslide_speed: 400,
+    autoslide_delay: 3000,
+    max_slide_height: 400,
     active_class: "is-active",
     previous_button_class: "c-banner-slider__button-previous",
     next_button_class: "c-banner-slider__button-next",
-    thumbs_class: "thumbs"
+    thumbs_class: "c-banner-slider-thumbs"
   };
 
   // The actual plugin constructor
@@ -57,11 +46,9 @@
       var $first_slide = $ul.find("li:first-child");
       var $last_slide = $ul.find("li:last-child");
       var slide_width = self.settings.max_slide_thumbs * 100.00;
-      var slide_height = 400;
       var slide_width_pc = 100.0 / self.settings.max_slide_thumbs;
       var slide_index = 0;
-      var new_slide_height = slide_height;
-      var thumb_height = slide_height;
+      var new_slide_height = self.settings.max_slide_height;
       var thumbs_array = [];
       var tag_class_RE = /^[A-Za-z][-_A-Za-z0-9]+$/;
 
@@ -91,8 +78,8 @@
       }
 
       // ensure auto-slide speed is integer
-      if (!$.isNumeric(self.settings.autoslide_speed)) {
-        self.settings.autoslide_speed = self._defaults.autoslide_speed;
+      if (!$.isNumeric(self.settings.autoslide_delay)) {
+        self.settings.autoslide_delay = self._defaults.autoslide_delay;
       }
 
       // ensure auto_slide is boolean
@@ -100,37 +87,8 @@
         self.settings.auto_slide = self._defaults.auto_slide;
       }
 
-
-      // Slide function
+      // Modify the DOM
       // ==========================
-      function slide(new_slide_index) {
-        var margin_left_pc = (new_slide_index * (-100) - 100) + "%";
-        $ul.animate({"margin-left": margin_left_pc}, 400, function () {
-          if (new_slide_index < 0) {
-            var left_magrin_set_to = (self.settings.max_slide_thumbs) * (-100);
-            $ul.css("margin-left", left_magrin_set_to + '%');
-            new_slide_index = self.settings.max_slide_thumbs - 1;
-          }
-          else if (new_slide_index >= self.settings.max_slide_thumbs) {
-            $ul.css("margin-left", "-100%");
-            new_slide_index = 0;
-          }
-          slide_index = new_slide_index;
-          styleActiveThumb(slide_index);
-        });
-
-      }
-
-      // Set active thumb function
-      // ==========================
-      function styleActiveThumb(new_index) {
-        $thumb_item.each(function (i) {
-          if (i === new_index) {
-            $thumb_item.removeClass(self.settings.active_class);
-            $(this).addClass(self.settings.active_class);
-          }
-        });
-      }
 
       // Remove any slides outside the defined max_number_slides settings default variable
       $ul.find("li:nth-child(n+" + (self.settings.max_slide_thumbs + 1) + ")").remove();
@@ -145,7 +103,6 @@
         width: slide_width + "%"
       });
 
-
       // Loop each image and set index,
       // positioning and build thumbs array
       // ==================================================
@@ -155,7 +112,8 @@
         var slide_left_percent = (slide_width_pc * index) + "%";
 
         // Add each image src to the thumbs array
-        thumbs_array.push(image_src);
+        self.thumbs_array = thumbs_array;
+        self.thumbs_array.push(image_src);
 
         // Give each slide its margin left %
         $(this).css({
@@ -165,7 +123,7 @@
 
         // Set the initial index to the element with "active" class
         if ($(this).hasClass(self.settings.active_class)) {
-          slide_index = index - 1;
+          self.settings.slide_index = index - 1;
         }
 
         // set the initial height of slide image height variable
@@ -174,27 +132,113 @@
         }
 
         // set the slide list height to the shortest slide image
-        if (new_slide_height < slide_height) {
-          $el.height(new_slide_height);
+        if (new_slide_height < self.settings.max_slide_height) {
+          $el.css("height", new_slide_height);
         }
         else {
-          $el.height(slide_height);
+          $el.css("height", self.settings.max_slide_height);
         }
 
       });
 
-      // Set up autoslide
+      // Build the thumbs first
+      self.buildThumbs();
+      // Init the slide
+      self.slide(slide_index);
+
+      // All the sliding
+      // ==========================
+      // Autoslide
       var auto_slide = setInterval(function () {
         if (self.settings.auto_slide) {
-          slide(slide_index + 1);
+          self.slide(self.slide_index + 1);
         }
-      }, self.settings.autoslide_speed);
+      }, self.settings.autoslide_delay);
 
-      // Generate thumbnail images
+      // Prev/next buttons
+      $("." + self.settings.previous_button_class).click(function () {
+        self.slide(self.slide_index - 1);
+        clearInterval(auto_slide);
+      });
+      $("." + self.settings.next_button_class).click(function () {
+        self.slide(self.slide_index + 1);
+        clearInterval(auto_slide);
+      });
+
+      // Swipe
+      $el.swipe({
+        // Single swipe handler for left swipes
+        swipeLeft: function () {
+          self.slide(self.slide_index + 1);
+          clearInterval(auto_slide);
+        },
+        swipeRight: function () {
+          self.slide(self.slide_index - 1);
+          clearInterval(auto_slide);
+        }
+      });
+
+      // Click a thumb
+      self.thumb_item.click(function () {
+        var thumb_index = $(this).index();
+        self.slide(thumb_index);
+        self.setActive(thumb_index);
+        clearInterval(auto_slide);
+      });
+    },
+    slide: function (new_slide_index) {
+      var self = this;
+      var $el = $(this.element);
+      var $ul = $el.find("ul");
+      var margin_left_pc = (new_slide_index * (-100) - 100) + "%";
+
+      $ul.animate({"margin-left": margin_left_pc}, self.settings.autoslide_speed, function () {
+        if (new_slide_index < 0) {
+          var left_magrin_set_to = (self.settings.max_slide_thumbs) * (-100);
+          $ul.css("margin-left", left_magrin_set_to + '%');
+          new_slide_index = self.settings.max_slide_thumbs - 1;
+        }
+        else if (new_slide_index >= self.settings.max_slide_thumbs) {
+          $ul.css("margin-left", "-100%");
+          new_slide_index = 0;
+        }
+        self.setActive(new_slide_index);
+        self.slide_index = new_slide_index;
+
+        return this;
+      });
+    },
+    setActive: function (new_index) {
+      var self = this;
+      var $el = $(this.element);
+      var $ul = $el.find('ul');
+      self.thumb_item.each(function (i) {
+        if (i === new_index) {
+          self.thumb_item.removeClass(self.settings.active_class);
+          $(this).addClass(self.settings.active_class);
+        }
+      });
+
+      $ul.find("li").each(function (i) {
+        if (i === new_index) {
+          $ul.find("li").removeClass(self.settings.active_class);
+          $(this).addClass(self.settings.active_class);
+        }
+      });
+
+      self.slide_index = new_index;
+      return this;
+    },
+    buildThumbs: function () {
+      var self = this;
+      var $el = $(this.element);
+      var thumbs_array = self.thumbs_array;
+      var thumb_height = self.settings.max_slide_height;
+
+      // Generate thumbs
       // ==========================
       // remove the cloned slides from the thumbs
       thumbs_array = thumbs_array.slice(1, -1);
-
       // Build the thumbs markup
       var thumb_list = "<div class='" + self.settings.thumbs_class + "'><ul class='" + self.settings.thumbs_class + "__list'>";
       for (var i = 0; i < thumbs_array.length; i++) {
@@ -204,6 +248,7 @@
       }
       thumb_list += "</ul></div>";
       $el.after(thumb_list);
+      // ==========================
 
       // variables defined after thumb markup generated
       var $thumbs = $("." + self.settings.thumbs_class);
@@ -221,41 +266,11 @@
         $thumbs.css("height", thumb_height);
       });
 
-      // Click a thumb
-      $thumb_item.click(function () {
-        var thumb_index = $(this).index();
-        slide(thumb_index);
-        clearInterval(auto_slide);
-      });
+      self.thumb_item = $thumb_item;
 
-      // Prev/next buttons
-      // ==========================
-      $("." + self.settings.previous_button_class).click(function () {
-        slide(slide_index - 1);
-        clearInterval(auto_slide);
-      });
-
-      $("." + self.settings.next_button_class).click(function () {
-        slide(slide_index + 1);
-        clearInterval(auto_slide);
-      });
-
-      // Add swipe functionality
-      // ==========================
-      $el.swipe({
-        // Single swipe handler for left swipes
-        swipeLeft: function () {
-          slide(slide_index + 1);
-          clearInterval(auto_slide);
-        },
-        swipeRight: function () {
-          slide(slide_index - 1);
-          clearInterval(auto_slide);
-        }
-      });
-
-      slide(slide_index);
+      return this;
     }
+
   });
 
   $.fn[pluginName] = function (options) {
